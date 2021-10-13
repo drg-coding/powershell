4176be84465bec4582c379d2a8134bc8f1c9fc35
drg-coding <52427024+drg-coding@users.noreply.github.com>
2021-10-13 10:35:45 -0400

    Add files via upload
    Query computers in an OU and output to a csv file

---
+++ Get_Comps_in_OU_generic.ps1
@@ -0,0 +1,3 @@
+﻿$OUpath = 'ou=<>,ou=<>,ou=<>,ou=<>,ou=<>,dc=<>,dc=<>,dc=<>,dc=<>'
+$ExportPath = 'C:\<>.csv'
+Get-ADComputer -Filter * -SearchBase $OUpath | Select-Object DistinguishedName,DNSHostName,Name | Export-Csv -NoType $ExportPath
